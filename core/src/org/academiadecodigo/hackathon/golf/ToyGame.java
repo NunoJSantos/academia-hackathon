60311f23b9227a090297a9ad83c8878ab62a6deb
neryjoao <joaonery1@gmail.com>
2018-07-26 20:40:23 +0100

    Simple menu added

--- core/src/org/academiadecodigo/hackathon/golf/ToyGame.java
+++ core/src/org/academiadecodigo/hackathon/golf/ToyGame.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.hackathon.golf;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -11,7 +12,7 @@ import com.badlogic.gdx.utils.*;
 
 import java.lang.reflect.Array;
 
-public class ToyGame extends ApplicationAdapter {
+public class ToyGame implements Screen {
 	SpriteBatch batch;
 	Texture img;
 	private Toy toy;
@@ -19,11 +20,13 @@ public class ToyGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private Weapon weapon;
 
+	private TheGame game;
 
     private Projectile bottles;
 
-	@Override
-	public void create () {
+	public ToyGame(TheGame game) {
+		this.game = game;
+
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1024,768);
 		batch = new SpriteBatch();
@@ -31,32 +34,58 @@ public class ToyGame extends ApplicationAdapter {
 		sensualWoman = new SensualWoman();
 		weapon = new Weapon(toy);
 
-        bottles = new Projectile();
-        bottles.spawnBottles();
+		bottles = new Projectile();
+		bottles.spawnBottles();
 
 		//img = new Texture(name of grid); --> put image of background
+
 	}
 
 	@Override
-	public void render () {
+	public void show() {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void render (float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(weapon.getWeaponTexture(),weapon.getWeapon().getX(),weapon.getWeapon().getY());
-		batch.draw(toy.getToyImage(), toy.getToy().getX(), toy.getToy().getY());
-		batch.draw(sensualWoman.getWomanImage(), ((float)(sensualWoman.getSensualWoman().getX())) ,((float)sensualWoman.getSensualWoman().getY()) );
+		game.batch.begin();
+		game.batch.draw(weapon.getWeaponTexture(),weapon.getWeapon().getX(),weapon.getWeapon().getY());
+		game.batch.draw(toy.getToyImage(), toy.getToy().getX(), toy.getToy().getY());
+		game.batch.draw(sensualWoman.getWomanImage(), ((float)(sensualWoman.getSensualWoman().getX())) ,((float)sensualWoman.getSensualWoman().getY()) );
 
         for(Rectangle bottle: bottles.getBottles()){
-            batch.draw(bottles.getBottleImage(), bottle.x, bottle.y);
+            game.batch.draw(bottles.getBottleImage(), bottle.x, bottle.y);
         }
-		batch.draw(sensualWoman.getWomanImage(), ((sensualWoman.getSensualWoman().getX())) ,(sensualWoman.getSensualWoman().getY()) );
-		batch.end();
+		game.batch.draw(sensualWoman.getWomanImage(), ((sensualWoman.getSensualWoman().getX())) ,(sensualWoman.getSensualWoman().getY()) );
+		game.batch.end();
 		camera.update();
 
 		toy.move();
 		weapon.move(toy);
 
-        batch.setProjectionMatrix(camera.combined);
+        game.batch.setProjectionMatrix(camera.combined);
 
         bottles.checkTime();
 
@@ -72,7 +101,6 @@ public class ToyGame extends ApplicationAdapter {
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
         bottles.dispose();
 	}
 }
